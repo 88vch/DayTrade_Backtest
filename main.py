ebef56e90084aa6d21ebb9087b4b434455959f68
Zach Qin <zvchqin@gmail.com>
2024-03-25 19:24:32 -0700

    created main file

---
+++ main.py
@@ -0,0 +1,14 @@
+import crawler
+import notion
+
+
+if __name__ == "__main__":
+    pass
+    # 1. run crawler after market close on $(STOCK)
+    # 2. load data from $(STOCK) to dict()
+    # 3. check if setups are viable on day's chart
+    # 4. upload results back to notion
+
+    # Extra. create chart from data
+    # - download/save chart locally (e.g., charts/*) as type(img)
+    # - if we can do this, we will simply make another notion integration to upload the chart there
